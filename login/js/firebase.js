e3aed2f6829295f160970cca35dc51467e385da9
Nelson Tovar <ntovar@fiec.espol.edu.ec>
2024-10-17 21:08:38 -0700

    1st commit

---
+++ login/js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD09-AvJMNgOAKG9tm4wZw8LgMlZmmpUJg",
+  authDomain: "ist105midtermexam-nelsontovars.firebaseapp.com",
+  projectId: "ist105midtermexam-nelsontovars",
+  storageBucket: "ist105midtermexam-nelsontovars.appspot.com",
+  messagingSenderId: "62233803631",
+  appId: "1:62233803631:web:311b22e296ba6b717dc69a",
+  measurementId: "G-CGMTY7RVB9"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
